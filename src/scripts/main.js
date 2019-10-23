4908ab8deeab8dd5748959dec26db1d626093328
Serhii-P <serhii.pyskun@gmail.com>
2019-10-23 15:27:24 +0300

    'Solution'

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,27 +1,27 @@
 'use strict';
 
-/* конфигурация */
-const width = 260; // ширина картинки
-const count = 3; // видимое количество изображений
+/* configuration */
+const width = 260; // img width
+const count = 3; // visible images
 const slider = document.getElementById('carousel');
 
 const list = slider.querySelector('ul');
 const listElems = slider.querySelectorAll('li');
 
-let position = 0; // положение ленты прокрутки
+let position = 0; // start point
 
 slider.querySelector('.prev').onclick = function() {
-  // сдвиг влево
+  // align left
   position += width * count;
-  // последнее передвижение влево может быть не на 3, а на 2 или 1 элемент
+  // last img move could be on 1,2,3.. element
   position = Math.min(position, 0);
   list.style.marginLeft = position + 'px';
 };
 
 slider.querySelector('.next').onclick = function() {
-  // сдвиг вправо
+  // align ightr
   position -= width * count;
-  // последнее передвижение вправо может быть не на 3, а на 2 или 1 элемент
+  //  last img move could be on 1,2,3.. element
   position = Math.max(position, -width * (listElems.length - count));
   list.style.marginLeft = position + 'px';
 };
